4af764d39b204462829d00ee070ba6481a43d3e9
devon gibson <devongibs2@gmail.com>
2022-02-20 00:01:40 -0800

    added assign

---
+++ code3.js
@@ -0,0 +1,14 @@
+var ages = [3, 9, 23, 64, 2, 8, 28, 93 ];
+
+var length =ages [ages.length-1] ;
+ 
+ages[8] = 22 ;
+
+console.log (ages);
+
+
+ var names = [ 'Sam','Tommy','Tim',"Sally", 'Buck', 'Bob'];
+
+ for ( let  i= 0; i < names.length; i++) {
+     console.log (i);
+ }
